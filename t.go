916b65af65363d292b6ab60f6868105bb1c2fd7d
Jacob Wisniewski <wisniewskij26@gmail.com>
2017-05-12 17:59:07 -0500

    added method to calculate the pooled variance t statistic

--- t.go
+++ t.go
@@ -1,1 +1,14 @@
 package ava
+
+import "math"
+
+// PooledVarianceTStat calculates the t statistic to be used in the comparison of two means.
+func PooledVarianceTStat(groups [][]float64) float64 {
+	x1 := average(groups[0])
+	n1 := float64(len(groups[0]))
+	x2 := average(groups[1])
+	n2 := float64(len(groups[1]))
+	s := pooledVariance(groups)
+	denom := math.Sqrt(s * ((1 / n1) + (1 / n2)))
+	return (x1 - x2) / denom
+}
--- t_test.go
+++ t_test.go
@@ -1,1 +1,16 @@
 package ava
+
+import "testing"
+
+func TestPooledVarianceTStat(t *testing.T) {
+	data := [][]float64{
+		[]float64{22, 34, 52, 62, 30, 40, 64, 84, 56, 59},
+		[]float64{52, 71, 76, 54, 67, 83, 66, 90, 77, 84},
+	}
+	actual := PooledVarianceTStat(data)
+	expected := -3.044550122546798
+
+	if actual != expected {
+		t.Errorf("PooledVarianceTStat: Expected %+v, got %+v", expected, actual)
+	}
+}
